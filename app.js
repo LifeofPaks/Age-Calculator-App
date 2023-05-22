e322c6c590e9c284ed0d3d5c303b85f36624439c
Paks <promise.chukwudi@yahoo.com>
2023-05-22 11:37:14 +0100

    Age Calculator Complete update1

--- app.js
+++ app.js
@@ -1,77 +1,127 @@
 
-
+// VARIABLE DECLARATIONS===============================================
 const dayInput = document.querySelector('.day-input')
 const monthInput = document.querySelector('.month-input')
 const yearInput = document.querySelector('.year-input')
+
 const displayYear = document.querySelector('.display-year')
 const displayMonth = document.querySelector('.display-month')
 const displayDay = document.querySelector('.display-day')
-const dateBox = document.querySelector('.date-box')
-const box  = document.querySelector('.box ')
+const submitBtn = document.querySelector('.submit')
+const yearBox  = document.querySelector('.year-box ')
+const monthBox  = document.querySelector('.month-box ')
+const dayBox  = document.querySelector('.day-box ')
 
-// const invalidDay  =document.querySelector('.valid-day')
-// const invalidMonth = document.querySelector('.valid-month')
-// // const fieldIsEmpty = document.querySelector('.error-empty')
-// const invalidYear = document.querySelector('.valid-year')
-// const invalidDate = document.querySelector('.valid-date')
-
-// const dayBox = document.querySelector('.day-box')
-// const monthBox = document.querySelector('.month-box')
-// const yearBox  = document.querySelector('.year-box ')
+const invalidDay  =document.querySelector('.valid-day')
+const invalidMonth = document.querySelector('.valid-month')
+const invalidYear = document.querySelector('.valid-year')
 
 
 
-dateBox.addEventListener('submit', e=>{
-    e.preventDefault()
-    validateAge()
+let isValid = false
+
+// DAY INPUT EVENTLISTENER===============================================
+dayInput.addEventListener('input', e=>{
+    if (+dayInput.value > 31){
+        invalidDay.innerHTML = 'Must be a valid day'
+        dayBox.classList.add('error')
+        dayBox.classList.add('title-error')
+        isValid = false
+
+        return
+    }
+
+    if(+dayInput.value === 0){
+        isValid = false
+        invalidDay.innerHTML = 'This field is required'
+        dayBox.classList.add('error')
+        dayBox.classList.add('title-error')
+        isValid = false
+
+        return
+    }
+    else{
+        invalidDay.innerHTML = ''
+        dayBox.classList.remove('error')
+        dayBox.classList.remove('title-error')
+        isValid = true
+    }
 })
 
-function validateAge(){
-    const dayInputValue = dayInput.value.trim()
-    const monthInputValue = monthInput.value.trim()
-    const yearInputValue = yearInput.value.trim()
 
-    if(dayInputValue === ''){
-        setErrorEmptyField(dayInput, 'This field is required')
-    } else{
-        setSuccess(dayInput)
+// MONTH INPUT EVENTLISTENER===============================================
+monthInput.addEventListener('input', e=>{
+    if (+monthInput.value > 12){
+        invalidMonth.innerHTML = 'Must be a valid month'
+        monthBox.classList.add('error')
+        monthBox.classList.add('title-error')
+        isValid = false
+
+        return
     }
 
+    if(+monthInput.value === 0){
+        isValid = false
+        invalidMonth.innerHTML = 'This field is required'
+        monthBox.classList.add('error')
+        monthBox.classList.add('title-error')
+        isValid = false
 
-    if(monthInputValue === ''){
-        setErrorEmptyField(monthInput, 'This field is required')
-    } else{
-        setSuccess(monthInput)
+        return
+    }
+    else{
+        invalidMonth.innerHTML = ''
+        monthBox.classList.remove('error')
+        monthBox.classList.remove('title-error')
+        isValid = true
+    }
+})
+
+// YEAR INPUT EVENTLISTENER===============================================
+yearInput.addEventListener('input', e=>{
+    if (+yearInput.value > 2023){
+        invalidYear.innerHTML = 'Must be a valid year'
+        yearBox.classList.add('error')
+        yearBox.classList.add('title-error')
+        isValid = false
+
+        return
     }
 
+    if(+yearInput.value === 0){
+        isValid = false
+        invalidYear.innerHTML = 'This field is required'
+        yearBox.classList.add('error')
+        yearBox.classList.add('title-error')
+        isValid = false
 
-
-    if(yearInputValue === ''){
-        setErrorEmptyField(monthInput, 'This field is required')
-    } else{
-        setSuccess(monthInput)
+        return
     }
+    else{
+        invalidYear.innerHTML = ''
+        yearBox.classList.remove('error')
+        yearBox.classList.remove('title-error')
+        isValid = true
+    }
+})
 
-}
+// SUBMIT BUTTON EVENTLISTENER===============================================
+
+submitBtn.addEventListener('click', calculateDate)
 
 
-function setError(element, message){
+function calculateDate(){
+    let birthday = `${monthInput.value}/ ${dayInput.value}/${yearInput.value}`
+    let birthdayObj = new Date(birthday)
+    let ageDiff = Date.now() - birthdayObj
+    let ageDate = new Date(ageDiff)
+    let ageYear = ageDate.getUTCFullYear() - 1970
+    let ageMonth = ageDate.getUTCMonth()
+    let ageDay = ageDate.getUTCDay() - 1
 
-    const box = element.parentElement
-    const small = box.querySelector('small')
-    small.innerHTML = message
-    small.style.display = 'block'
-    
-    box.classList.add('input-error')
-    box.classList.remove('input-success')
-
-}
-
-function setSuccess(element){
-    const box = element.parentElement
-
-    box.classList.remove('input-error')
-    box.classList.add('input-success')
+    displayYear.innerHTML = ageYear
+    displayMonth.innerHTML = ageMonth
+    displayDay.innerHTML = ageDay
 }
 
 
